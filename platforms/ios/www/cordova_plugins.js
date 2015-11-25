f0510cb980e3c5b8fa956c5ca05b40198fb92874
Michael Kubovic <michael.kubovic@gmail.com>
2015-11-25 19:34:13 +0100

    initial commit

---
+++ platforms/ios/www/cordova_plugins.js
@@ -0,0 +1,19 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/cordova-plugin-googleplus/www/GooglePlus.js",
+        "id": "cordova-plugin-googleplus.GooglePlus",
+        "pluginId": "cordova-plugin-googleplus",
+        "clobbers": [
+            "window.plugins.googleplus"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "cordova-plugin-whitelist": "1.2.0",
+    "cordova-plugin-googleplus": "4.0.5"
+}
+// BOTTOM OF METADATA
+});
